ac91ac5ccec0209c27b053eeae9caf81a9f790ba
saleh <saleh@alghusson.com>
2022-03-09 13:54:07 -0500

    remove border from footer

--- pages/index.js
+++ pages/index.js
@@ -62,7 +62,6 @@ export default function Home() {
         footer {
           width: 100%;
           height: 50px;
-          border-top: 1px solid #eaeaea;
           display: flex;
           justify-content: center;
           align-items: center;
