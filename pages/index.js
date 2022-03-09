f2649040db77281e759f618db1c18bc5b7fab393
saleh <saleh@alghusson.com>
2022-03-09 07:48:16 -0500

    anna_v2

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,131 @@
 import Head from 'next/head'
 
-import Post from '../components/post'
-
-export async function getStaticProps() {
-  // fetch list of posts
-  const response = await fetch(
-    'https://jsonplaceholder.typicode.com/posts?_page=1'
-  )
-  const postList = await response.json()
-  return {
-    props: {
-      postList,
-    },
-  }
-}
-
-export default function IndexPage({ postList }) {
+export default function Home() {
   return (
-    <main>
+    <div className="container">
       <Head>
-        <title>Home page</title>
+        <title>Anna Kukla</title>
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>List of posts</h1>
+      <video id="background-video" loop autoPlay muted>
+        <source
+          src="https://d2qxpcg73pnz75.cloudfront.net/annatv_9mb.mp4"
+          type="video/mp4"
+        />
+        Your browser does not support the video tag.
+      </video>
+      <main>
 
-      <section>
-        {postList.map((post) => (
-          <Post {...post} key={post.id} />
-        ))}
-      </section>
-    </main>
+
+        <h1 className="title">
+          Anna Kukla
+        </h1>
+
+      </main>
+
+      <footer>
+        © Copyright {new Date().getFullYear()} Anna Kukla
+      </footer>
+
+      <style jsx>{`
+        #background-video{
+          height: 100%;
+          width: 100%;
+          float: left;
+          top: 0;
+          padding: none;
+          position: fixed; /* optional depending on what you want to do in your app */
+          z-index: 0;
+        }
+        .container {
+          min-height: 100vh;
+          padding: 0 0.5rem;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          z-index: 1;
+        }
+
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          z-index: 1;
+        }
+
+        footer img {
+          margin-left: 0.5rem;
+        }
+
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
+
+        .title a {
+          color: #0070f3;
+          text-decoration: none;
+        }
+
+        .title a:hover,
+        .title a:focus,
+        .title a:active {
+          text-decoration: underline;
+        }
+
+        .title {
+          margin: 0;
+          line-height: 1.15;
+          font-size: 4rem;
+        }
+
+        .title {
+          text-align: center;
+        }
+
+        .logo {
+          height: 1em;
+        }
+
+        @media (max-width: 600px) {
+
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
   )
 }
