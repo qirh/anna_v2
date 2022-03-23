b31f428e8f0e14262038b800050caebfdbf0de8c
saleh <saleh@alghusson.com>
2022-03-22 21:13:36 -0400

    css changes

--- pages/index.js
+++ pages/index.js
@@ -22,16 +22,27 @@ export default function Home() {
       ` }}></div>
 
       <main>
+        <section class="section-header">
+          <div class="name-holder">
+            <h1>Anna Kukla</h1>
+            <h2>Artist / Costumer in NYC </h2>
+          </div>
 
-        <img
-          src="https://d2qxpcg73pnz75.cloudfront.net/one-crop.png"
-          className="image"
-          ></img>
+          <div class="img-holder">
+            <img
+              src="https://d2qxpcg73pnz75.cloudfront.net/one-crop.png"
+              className="image"
+            ></img>
+          </div>
+        </section>
 
-        <span>
+        <section class="section-links" >
+        </section>
+
+        <section class="section-long">
           hey! I'm Anna Kukla. I work the most amazing and best Costumer in the whole world. I specialise in Film & TV and have worked on some pretty cool projets.
           If you would liek to connect. This is my LinkedIn, IMDB and CV. I'm also reachable over email
-        </span>
+        </section>
 
       </main>
 
@@ -39,52 +50,6 @@ export default function Home() {
         Made with 💙 in NYC
       </footer>
 
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          /* mobile viewport bug fix */
-          min-height: -webkit-fill-available;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-          z-index: 1;
-        }
-
-        footer {
-          width: 100%;
-          height: 50px;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          z-index: 1;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .image {
-          height: 350px;
-          width: 350px;
-          opacity: 0.9;
-        }
-
-        @media (max-width: 600px) {
-
-        }
-      `}</style>
 
       <style jsx global>{`
         html,
@@ -116,6 +81,51 @@ export default function Home() {
           top: 0;
           left: 0;
           z-index: 0;
+        }
+        .container {
+          min-height: 100vh;
+          /* mobile viewport bug fix */
+          min-height: -webkit-fill-available;
+          padding: 0 0.5rem;
+          display: flex;
+          flex-direction: column;
+        }
+
+        main { 
+          z-index: 1;
+          margin: 50px auto;
+          padding: 5px;
+          max-width: 650px;
+        }
+        .section-header {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-wrap: wrap;
+        }
+
+        footer {
+          width: 100%;
+          height: 50px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          z-index: 1;
+        }
+
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
+
+        .image {
+          height: 350px;
+          width: 350px;
+          opacity: 0.9;
+        }
+
+        @media (max-width: 600px) {
+
         }
         
       `}</style>
