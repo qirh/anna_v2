34536d21c91215c389f84b64263d0c5f331e88ce
saleh <saleh@alghusson.com>
2022-04-02 23:39:59 -0400

    new style

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function Home() {
   return (
@@ -8,8 +8,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div dangerouslySetInnerHTML={{
-        __html: `
+      <div
+        className="video"
+        dangerouslySetInnerHTML={{
+          __html: `
         <video
           loop
           muted
@@ -19,59 +21,44 @@ export default function Home() {
           src="https://d2qxpcg73pnz75.cloudfront.net/annatv_6mb.mp4"
           class="background-video"
         />,
-      ` }}></div>
+      `,
+        }}
+      ></div>
 
-      <main>
-        <section class="section-header">
-          <div class="name-holder">
-            <h1>Anna Kukla</h1>
-            <h2>Artist / Costumer in NYC </h2>
-          </div>
-
-          <div class="img-holder">
-            <img
-              src="https://d2qxpcg73pnz75.cloudfront.net/one-crop.png"
-              className="image"
-            ></img>
-          </div>
-        </section>
-
-        <section class="section-links" >
-        </section>
-
-        <section class="section-long">
-          hey! I'm Anna Kukla. I work the most amazing and best Costumer in the whole world. I specialise in Film & TV and have worked on some pretty cool projets.
-          If you would liek to connect. This is my LinkedIn, IMDB and CV. I'm also reachable over email
-        </section>
-
-      </main>
-
-      <footer>
-        Made with 💙 in NYC
-      </footer>
-
+      <div className="name">
+        <h1>Anna Kukla</h1>
+      </div>
+      
+      <div className="links">
+        <a href="https://www.imdb.com/name/nm7706290/">IMDB</a>
+        <a href="https://drive.google.com/file/d/1qwZzVOZ4PbAedrvirdGsIDDWAoAwFOoJ">
+          Resume
+        </a>
+        <a href="mailto:kukla.anna.kukla@gmail.com">Email</a>
+      </div>
 
       <style jsx global>{`
+        * {
+          box-sizing: border-box;
+        }
+
         html,
         body {
           padding: 0;
           margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        body {
           min-height: 100vh;
-          /* mobile viewport bug fix */
-          min-height: -webkit-fill-available;
-        }
-        
-        html {
-          height: -webkit-fill-available;
         }
 
-        * {
-          box-sizing: border-box;
+        .video,
+        .background-video {
+          z-index: 0;
+        }
+
+        .container {
+          min-height: 100vh;
+          min-width: 100vh;
+          position: relative;
+          overflow: hidden;
         }
         .background-video {
           object-fit: cover;
@@ -80,55 +67,31 @@ export default function Home() {
           position: fixed;
           top: 0;
           left: 0;
-          z-index: 0;
-        }
-        .container {
-          min-height: 100vh;
-          /* mobile viewport bug fix */
-          min-height: -webkit-fill-available;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
         }
 
-        main { 
-          z-index: 1;
-          margin: 50px auto;
-          padding: 5px;
-          max-width: 650px;
-        }
-        .section-header {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            flex-wrap: wrap;
-        }
-
-        footer {
-          width: 100%;
-          height: 50px;
-          display: flex;
-          justify-content: center;
-          align-items: center;
+        .name,
+        .links {
+          position: absolute;
           z-index: 1;
         }
-
+        .name {
+          top: 80%;
+          left: 20%;
+        }
+        .links {
+          top: 90%;
+          left: 35%;
+        }
+        .links a:not(:last-child) {
+          padding: 0 12px 0 0;
+        }
         a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .image {
-          height: 350px;
-          width: 350px;
-          opacity: 0.9;
+          color: rgb(16, 162, 245);
         }
 
         @media (max-width: 600px) {
-
         }
-        
       `}</style>
     </div>
-  )
+  );
 }
