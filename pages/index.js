34abca82bbe98cac47e41beeb476267f0264d91a
saleh <saleh@alghusson.com>
2022-04-05 18:22:24 -0400

    fix favicon issue in safari

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,10 @@ export default function Home() {
         <title>Anna K</title>
         <meta name="theme-color" content="#4d5b7c" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       </Head>
 
       <div
